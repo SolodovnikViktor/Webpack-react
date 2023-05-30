523a1132e17a50da9cc23d8740d719aa4659497c
Виктор <vsolodovnik88@mail.ru>
2023-05-30 22:11:19 +0300

    dev

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Header } from '../shared/Header';
+import { Header } from './Header';
 
 window.addEventListener('load', () => {
-  ReactDOM.hydrate(<Header />, document.getElementById('react_root'));
+  ReactDOM.render(<Header />, document.getElementById('react_root'));
 });
